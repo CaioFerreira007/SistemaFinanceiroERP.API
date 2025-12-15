e9b6fa784d55089127094870cf61334663ab15d2
Caio Gustavo <caiogggustavo49@gmail.com>
2025-12-14 23:07:26 -0300

    DTO produto criado

---
+++ SistemaFinanceiroERP.API/DTOs/Produto/ProdutoCreateDto.cs
@@ -0,0 +1,21 @@
+﻿namespace SistemaFinanceiroERP.API.DTOs.Produto
+{
+    public class ProdutoCreateDto
+    {
+        public string ProdutoNome { get; set; } = string.Empty;
+
+        public string Categoria { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public decimal PrecoUnitario { get; set; }
+
+        public string CodigoBarras { get; set; } = string.Empty;
+
+        public int QuantidadeEstoque { get; set; }
+
+        public string UnidadeMedida { get; set; } = string.Empty;
+
+        public int EmpresaId { get; set; }
+    }
+}
