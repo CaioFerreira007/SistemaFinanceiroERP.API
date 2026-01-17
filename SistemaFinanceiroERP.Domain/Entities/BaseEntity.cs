0b98c819010deab01ddc1865ee85347dc6e02f33
Caio Gustavo <caiogggustavo49@gmail.com>
2026-01-17 12:44:58 -0300

    pequenos ajustes

--- SistemaFinanceiroERP.Domain/Entities/BaseEntity.cs
+++ SistemaFinanceiroERP.Domain/Entities/BaseEntity.cs
@@ -11,7 +11,7 @@ namespace SistemaFinanceiroERP.Domain.Entities
         public int Id { get; set; }
         public DateTime DataCriacao { get; set; }
 
-        public DateTime? DataAtualizacao{ get; set; }
+        public DateTime DataAtualizacao{ get; set; }
 
         public bool Ativo { get; set; } = true;
     }
